f8c269f45ae3c837c68b0cf4a7c65420334221e9
Milos Trifunovic <milos.trifunovic@gmail.com>
2014-11-17 21:18:10 +0100

    Fix: many small fixes
    BTC search was not optimized, many useless try/catch lines, re usage is
    improved

--- Project_203.py
+++ Project_203.py
@@ -60,11 +60,12 @@ class Mission:
                 elif f.endswith(".ext"):
                     self.ext_file[os.path.join(dirpath, f)] = os.path.basename(f)
                 elif f == '=BTC=_revive_init.sqf':
-                    self.btc_file[os.path.join(dirpath, f)] = os.path.basename(f)
+                    if 'lite' not in dirpath:
+                        self.btc_file[os.path.join(dirpath, f)] = os.path.basename(f)
                 elif f == 'FAR_revive_init.sqf':
                     self.far_file[os.path.join(dirpath, f)] = os.path.basename(f)
                 
-         print self.sqm_file, self.ext_file, self.btc_file
+         print self.sqm_file, self.ext_file, self.btc_file, self.far_file
          
          self.player_count = 0
          self.island  = 'unknown_island'
@@ -76,19 +77,29 @@ class Mission:
          
      def examine_sqm(self):
          
+         if self.sqm_file == {}:
+             return None
+         
          watch_for_addons = False
          new_sqm = 'new_sqm.sqm'
+         
+         overview_text_check = re.compile('\s*overviewText[ =]', re.IGNORECASE)
          
          infile = open(self.sqm_file.keys()[0], 'r')
          outfile = open(new_sqm, 'w')
          
          for line in infile:
+                # erasing briefing name
                 if 'briefingName="' in line or 'briefingName' in line:
                     print 'briefing name erased in .sqm!'
                     line = line.replace(line,'        briefingName="";\n')
+                    
+                # counting number of players
                 elif 'player="' in line:
                     print 'counting players', self.player_count
                     self.player_count +=1
+                    
+                # checking addons
                 elif ('addOns[]=' in line or 'addOnsAuto[]=' in line) and self.addons_on == False:
                     watch_for_addons = True
                     print 'addons: ', self.addons_on 
@@ -101,24 +112,35 @@ class Mission:
                 elif watch_for_addons == True and self.addons_on == False:  
                     if ('"a3_') not in line and ('"A3_') not in line:
                         if '{' not in line: 
-                            self.addons_on = True                      
+                            self.addons_on = True
+                            
+                # check if mission description is inside sqm
+                elif overview_text_check.match(line):
+                    des = line.split("=")[1]
+                    self.mission_des = self.mission_description(des)
+                    print 'mission description is ', self.mission_des 
+                                         
                 outfile.write(line)
          infile.close()
          outfile.close()
          sqm_data = [self.player_count, self.island, self.addons_on]              
-         return sqm_data, new_sqm
+         return [sqm_data, new_sqm]
          
      def examine_ext(self):
          ''' Method to handle data from ext file, erase briefing name, set 
          respawn to group, find game type, mission name, description, 
          author's name'''
          
+         if self.ext_file == {}:
+             return None
+         
          game_types = ['coop','dm','tdm','ctf','sc','cti','rpg','seize','defend',
-         'zdm','zctf','zcoop','zsc','zcti','ztdm','zrpg','zgm','zvz','zvp']
+         'zdm','zctf','zcoop','zsc','zcti','ztdm','zrpg','zgm','zvz','zvp', 'sp']
         
          respawn_check = re.compile('respawn[ =]', re.IGNORECASE)
-         on_load_name_check = re.compile('onLoadName =', re.IGNORECASE)
-         on_load_mission_check = re.compile('onLoadMission =', re.IGNORECASE)
+         on_load_name_check = re.compile('onLoadName[ =]', re.IGNORECASE)
+         on_load_mission_check = re.compile('onLoadMission[ =]', re.IGNORECASE)
+         overview_text_check = re.compile('overviewText[ =]', re.IGNORECASE)
          
          new_ext = 'new_ext.ext'
          infile = open(self.ext_file.keys()[0], 'r')
@@ -138,32 +160,22 @@ class Mission:
                         self.mission_name = load_name.group()[3:]
                         in_brackets = re.compile(r'\[[^)]*\]')
                         self.mission_name = re.sub(in_brackets, '', self.mission_name)
-                        self.mission_name = self.mission_name.replace(' ','_')
-                        self.mission_name = self.mission_name.translate(None, '";,*=-()&#/<>|')
-                        self.mission_name = self.mission_name.lower()
-                    else:
-                        self.mission_name = 'no_name'    
+                        self.mission_name = self.mission_name.split(';')[0].lower()
+                        self.mission_name = self.mission_name.rstrip('_').replace('__','_').replace(' ','_')
+                        self.mission_name = self.mission_name.translate(None, '";,*=-()\'.&#/<>|')  
                     print 'mission name is ', self.mission_name
                 
                 # checking mission description
-                elif on_load_mission_check.match(line):
-                    des = re.search(' (.*$)', line)
-                    if des != None:
-                        self.mission_des = des.group()[3:]
-                        self.mission_des = self.mission_des.translate(None, '";')
-                    else:
-                        self.mission_des = 'no description'    
+                elif overview_text_check.search(line) or on_load_mission_check.search(line):
+                    print line
+                    des = line.split("=")[1]
+                    self.mission_des = self.mission_description(des)
                     print 'mission description is ', self.mission_des
                 
                 # checking respawn type    
                 elif respawn_check.match(line): 
-                    #print 'setting respawn'
-                    #if re.search(r'[0-5]', line) != None:
+                    print 'setting respawn'
                     line = line.replace(line,'Respawn = 4;\n')
-                    #    print line
-                    #else: 
-                    #    line = line.replace(line,'Respawn = "GROUP";')
-                    #    print line
                     
                 # checking game type        
                 elif "gametype =" in line.lower():
@@ -185,57 +197,75 @@ class Mission:
          infile.close()
          outfile.close()       
          ext_data = [self.mission_name, self.mission_des, self.game_type]       
-         return ext_data, new_ext
+         return [ext_data, new_ext]
          
      def examine_btc(self):
          ''' Method to disable BTC respawn '''
+         
+         if self.btc_file == {}:
+             return None
+             
          has_respawn_btc = False
          btc_0 = 'BTC_disable_respawn = 0;'
          btc_1 = 'BTC_disable_respawn = 1;'  
          
          new_btc = 'new_btc.sqm'
+         
          infile = open(self.btc_file.keys()[0], 'r')
+         source = self.btc_file.keys()[0]
          outfile = open(new_btc, 'w')
          
          for line in infile:
-                if btc_0 in line:
-                    print 'found BTC'
-                    line = line.replace(line, '%s\n' % (btc_1))
-                    print 'BTC line replacement: ', line
-                    has_respawn_btc = True
-                    print 'BTC: ' + str(has_respawn_btc)
-                outfile.write(line)            
+            if btc_0 in line:
+                print 'found BTC'
+                line = line.replace(line, '%s\n' % (btc_1))
+                print 'BTC line replacement: ', line
+                has_respawn_btc = True
+                print 'BTC: ' + str(has_respawn_btc)
+            outfile.write(line)            
          infile.close()
          outfile.close()
-         return new_btc
+         return [source, new_btc]
          
      def examine_far(self):
          ''' Method to change FAR respawn to option 3 '''
+         
+         if self.far_file == {}:
+             return None
+         
          has_revive_far = False
-         far_1 = 'FAR_ReviveMode = 0;'
-         far_2 = 'FAR_ReviveMode = 1;'
-         far_3 = 'FAR_ReviveMode = 2;'  
+         far = 'FAR_ReviveMode ='
+         far_2 = 'FAR_ReviveMode = 2;' 
          
          new_far = 'new_far.sqm'
          infile = open(self.far_file.keys()[0], 'r')
          outfile = open(new_far, 'w')
          
          for line in infile:
-                if far_1 in line or far_2 in line:
-                    line = line.replace(line, '%s\n' % (far_3))
-                    print 'FAR line replacement: ', line
-                    has_revive_far = True
-                    print 'FAR: ' + str(has_revive_far)
-                outfile.write(line)            
+            if far in line:
+                line = line.replace(line, '%s\n' % (far_2))
+                print 'FAR line replacement: ', line
+                has_revive_far = True
+                print 'FAR: ' + str(has_revive_far)
+            outfile.write(line)            
          infile.close()
          outfile.close()
          return new_far
          
+     def mission_description(self, des):
+         '''  method to extract mission description'''
+         if des != None or des != '':
+            des = des.translate(None, '";').strip()
+            if len(des) > len(self.mission_des):
+                self.mission_des = des
+         return self.mission_des 
+         
      def folder_name(self, original_folder_name):
          '''  Generate new folder name from the acquired data '''
          
-         islands = ['fallujah', 'chernarus', 'utes', 'zagrabad', 'takistan', 
-         'bystrica', 'imrali'] 
+         islands = ['chernarus', 'utes', 'zagrabad', 'takistan', 'bystrica', 
+         'bukovina', 'shapur', 'desert', 'sahrani', 'imrali', 'thirskw', 
+         'thirsk', 'namalsk', 'fallujah'] 
             
          game_type = self.game_type[:2]
          
@@ -250,6 +280,9 @@ class Mission:
              player_count = '0' + str(self.player_count)
          else:
              player_count = str(self.player_count)
+             
+         if player_count == '01' and game_type == 'un':
+             game_type = 'sp'
              
          # temporary solution: when game type is not recognized - it is set to coop
          if game_type == 'un':
@@ -267,18 +300,24 @@ class Mission:
          # if mission name is not found in ext, look for mission name in file name                               
          if self.mission_name == 'no_name' or self.mission_name == '':
              name = unquote(original_folder_name)
-             name = name.translate(None, '";,*=-()&#/<>|').replace(' ','_').split(".")[0].lower()
-             # remove everything that is inside "[]" brackets
+             name = name.translate(None, '";,*=-()&#/<>|').rstrip('_').replace('__','_').replace(' ','_').split(".")[0].lower()
+             self.mission_name = name
              in_brackets = re.compile(r'\[(^)]*\)')
-             self.mission_name = re.sub(in_brackets, '', name)
+             self.mission_name = re.sub(in_brackets, '', self.mission_name)
              in_brackets_sq = re.compile(r'\[[^)]*\]')
-             self.mission_name = re.sub(in_brackets_sq, '', name)
+             self.mission_name = re.sub(in_brackets_sq, '', self.mission_name)
              print 'name from filename is ', self.mission_name
               
             
-         # make new folder name using data found in files                
-         folder_name = game_type, str(addons), player_count, '_', self.mission_name, '.', self.island # not needed, Osku's Tool adds .pbo  , '.pbo'
+         # make new folder name using data found in files
+         if game_type == 'sp':
+             folder_name = game_type, str(addons), '_', self.mission_name, '.', self.island
+         else:                 
+             folder_name = game_type, str(addons), player_count, '_', self.mission_name, '.', self.island # not needed, Osku's Tool adds .pbo  , '.pbo'
+         
+         # final checks - removal of unwanted characters
          folder_name = ''.join(folder_name)
+         folder_name = folder_name.rstrip('_').replace('__','_')
          return folder_name
          
      def copy_and_replace(self, original_file, edit): 
@@ -295,12 +334,13 @@ class Mission:
             print 'warning: required files are not found'
         except WindowsError:
             print 'warning: folder with the same name already exists or bad filename'
-            folder_name = folder_name[:-4] + '.DUPLICATE'
+            folder_name = folder_name + '.DUPLICATE'
             os.rename(original_name, folder_name)    
             
      def mission_info(self):
          ''' print out mission info '''
          print '\n'
+         print '==========================================================================='
          print 'Mission name: ', self.mission_name 
          print 'Mission description: ', self.mission_des  
          print 'Player count: ', str(self.player_count)
@@ -308,21 +348,24 @@ class Mission:
          print 'Addons: ', str(self.addons_on)
          print 'Island: ', self.island
          print 'Author: ', self.author
+         print '==========================================================================='
                                 
 # hardcoded fullpath for testing purposes
 fullpath = "F:\Games\Arma 2\Osku's tool\input"
 
 def find_folders(fullpath):
-    '''  search for folders inside main folder  '''
+    ''' search for folders inside main folder '''
     directories = [fullpath + "\\" + x for x in os.listdir(fullpath)]
     return directories
 
 def unpack(fullpath):
+    ''' unpack mission files using Osku's Tool and CPBO '''
     oskus_input = os.path.dirname(fullpath)
     p = Popen("cmd.exe /c extract.bat", cwd=r"%s" % (oskus_input))
     stdout, stderr = p.communicate()
     
 def repack(fullpath):
+    ''' repack mission files using Osku's Tool and CPBO '''
     oskus_repack = os.path.dirname(fullpath)
     p = Popen("cmd.exe /c repack.bat", cwd=r"%s" % (oskus_repack))
     stdout, stderr = p.communicate()
@@ -331,10 +374,7 @@ def main():
     ''' calls function to search for missions and then process them one by one,
     copy files, rename folders, and write data to csv file''' 
     
-    #try:
     unpack(fullpath)
-    #except:
-    #    print 'Nothing to unpack or error occured'
                 
     list_file = open('new_missions_list.csv', 'wb')
     writer = csv.writer(list_file)
@@ -342,69 +382,41 @@ def main():
     start = find_folders(fullpath)
     
     for mis in start:
-        mis1 = Mission(mis)
+        
+        mission = Mission(mis)
         original_folder_name = os.path.basename(os.path.normpath(mis))
 
-        try:
-            sqm = mis1.examine_sqm()
-        except IndexError:
-            print 'no sqm file in directory or not unpacked file'
-            sqm = None
+        # start processing
+        sqm = mission.examine_sqm()
+        ext = mission.examine_ext()
+        btc = mission.examine_btc()
+        far = mission.examine_far()
+        print sqm, ext, btc, far
         
-        try:                        
-            ext = mis1.examine_ext()
-        except IndexError:
-            print 'no ext file in directory'
-            ext = None        
-        except WindowsError:
-            print 'no ext file in directory'
-            ext = None
-            
-        try:    
-            btc = mis1.examine_btc()
-        except IndexError:
-            print 'no btc file in directory'
-            btc = None
-            
-        try:    
-            far = mis1.examine_far()
-        except IndexError:
-            print 'no far file in directory'
-            far = None
-        
-        print sqm, ext
-        folder_name = mis1.folder_name(original_folder_name)    
+        # generate folder name
+        folder_name = mission.folder_name(original_folder_name)    
         print folder_name
-    
-        mis1.copy_and_replace(mis + '\\mission.sqm', sqm[1])
         
-        if ext != None:
-            try:
-                mis1.copy_and_replace(mis + '\\description.ext', ext[1])
-            except WindowsError:
-                print 'no ext file in directory'
-                
-        if btc != None:
-            try:
-                mis1.copy_and_replace(mis + '\\=BTC=_revive\\=BTC=_revive_init.sqf', btc)
-            except WindowsError:
-                print 'no sqf file in directory'
-                
-        if far != None:
-            try:
-                mis1.copy_and_replace(mis + '\\FAR_revive\\FAR_revive_init.sqf', far)
-            except WindowsError:
-                print 'no far file in directory'
+        # copy all files
+        mission.copy_and_replace(mis + '\\mission.sqm', sqm[1])
         
+        if ext:
+            mission.copy_and_replace(mis + '\\description.ext', ext[1])
+
+        if btc:    
+            mission.copy_and_replace(btc[0], btc[1])
+            
+        if far:
+            mission.copy_and_replace(mission.far_file.keys()[0], far)
+        
+        # rename folder
         folder_name = folder_name.translate(None, '!#$:;*,"=-')
-        mis1.modify_folders(mis, fullpath + '\\' + folder_name)
+        mission.modify_folders(mis, fullpath + '\\' + folder_name)
         
-        writer.writerow((mis1.player_count, folder_name, mis1.mission_des, mis1.author, '', '', '', mis1.island, original_folder_name))
+        # insert entry in mission list csv
+        writer.writerow((mission.player_count, folder_name, mission.mission_des, mission.author, '', '', '', mission.island.capitalize(), original_folder_name))
     list_file.close()
     
-    #try:
     repack(fullpath)
-    #except:
-    #    print 'Error occured'
     
 main()
