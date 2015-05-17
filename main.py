fde7c93a72291228e0013af970179c0f0349551c
Milos Trifunovic <milos.trifunovic@gmail.com>
2015-05-17 03:52:50 +0200

    Uncomment some lines

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import sys, os
 from parse import fetch 
-from window import *
+from window import Window
+from sys import argv
+from os import getcwd, listdir
 
 if __name__ == "__main__":
-
-    #if len(sys.argv)==1:
-    #    if sys.argv[1] == 'gui':
-    gui = Window()
-    #path = os.getcwd()
-    #print fullpath, 'aaaa'
-    #if fullpath == '':
-    #    fullpath = os.getcwd()
-    #print 'prosao'
-    #filenames = next(os.walk(fullpath))[2]
-    ##print filenames
-    #if "extract.bat" in filenames and "repack.bat" in filenames and "reset.bat" in filenames:
-    #    fullpath = fullpath  + "\\input"
-    ##else:
-    ##    fullpath = "F:\Games\Arma 2\Osku's tool\input"
-    ##print os.listdir(fullpath + "\\input")
-    #if os.listdir(fullpath) == []:
-    #    print "no files to work with!"
-    #else:
-    #    fetch(fullpath)
+    print argv
+    if len(argv) > 1:
+        if argv[1] == 'gui':
+            gui = Window()
+    path = getcwd()
+    if path == '':
+        fullpath = getcwd()
+    if listdir(path) == []:
+        print "no files to work with!"
+    else:
+        fetch(path)
